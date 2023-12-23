711fabd27fa397502d363a2b2aecdb9014d83965
Austin Palmer <44661648+austinjp17@users.noreply.github.com>
2023-12-22 20:14:48 -0600

    Delete scraper.py
    remove python

--- scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-payload = { 
-        'api_key': '0861bae719981ddf7ae64ddfcb5193ad', 
-        'url': 'https://www.redfin.com/zipcode/77532' 
-    } 
-r = requests.get('https://api.scraperapi.com/', params=payload)
-print(r.text)
